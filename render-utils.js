64bce4f4fad60aed5d2be5bbfa1556055bf82693
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-23 17:39:45 -0700

    starting create page

--- render-utils.js
+++ render-utils.js
@@ -1,13 +1,13 @@
-export function renderWorkshop(families) {
+export function renderBunny(families) {
     const div = document.createElement('div');
     div.classList.add('families-container');
     const h2 = document.createElement('h2');
     h2.textContent = families.name;
 
     const ul = document.createElement('ul');
-    for (let participant of families.participants){
+    for (let bunny of families.loving_families){
         const li = document.createElement('li');
-        li.textContent = `${participant.name}`;
+        li.textContent = `${bunny.name}`;
         ul.append(li);
     }
     div.append(h2, ul);
